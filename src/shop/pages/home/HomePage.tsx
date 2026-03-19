7b8be0ec57f8770d49a44e55f916a81b172db037
Diaz Tabares Brayan David <brayandiaz162@gmail.com>
2026-03-19 14:45:43 -0600

    CustomPagination implement

--- src/shop/pages/home/HomePage.tsx
+++ src/shop/pages/home/HomePage.tsx
@@ -1,11 +1,9 @@
-import { CustomJumbotron } from "@/shop/components/CustomJumbotron"
-import { Button } from "../../../components/ui/button"
+import { CustomJumbotron } from "@/shop/components/CustomJumbotron";
 
 export const HomePage = () => {
   return (
     <div>
       <CustomJumbotron title="Todos los productos"/>
-      <Button>Boton</Button>
     </div>
   )
 }
