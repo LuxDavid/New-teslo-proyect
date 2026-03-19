50ea35ae5ec72cba4c4007cc68a948782a484f3a
Diaz Tabares Brayan David <brayandiaz162@gmail.com>
2026-03-19 15:02:41 -0600

    assets y carpeta mock implemented

--- src/shop/pages/home/HomePage.tsx
+++ src/shop/pages/home/HomePage.tsx
@@ -1,9 +1,12 @@
+import { CustomPagination } from "@/components/custom/CustomPagination";
 import { CustomJumbotron } from "@/shop/components/CustomJumbotron";
 
 export const HomePage = () => {
   return (
     <div>
       <CustomJumbotron title="Todos los productos"/>
+
+      <CustomPagination totalPages={5}/>
     </div>
   )
 }
