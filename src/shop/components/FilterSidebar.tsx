948189e2eb20d27c8bea030df03efdfc181793aa
Diaz Tabares Brayan David <brayandiaz162@gmail.com>
2026-03-22 19:19:29 -0600

    FilterSidebar logic to url params implemented

--- src/shop/components/FilterSidebar.tsx
+++ src/shop/components/FilterSidebar.tsx
@@ -2,8 +2,31 @@ import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { Label } from "@/components/ui/label";
+import { useSearchParams } from "react-router";
 
 export const FilterSidebar = () => {
+
+  const [searchParams, setSearchParams]=useSearchParams();
+
+  const currentSizes= searchParams.get('sizes')?.split(',') || [];
+  const currentPrice= searchParams.get('price') || 'any';
+
+  const handleSizeChange= (size: string) => {
+
+    const newSizes= currentSizes.includes(size) ?
+      currentSizes.filter(s => s !== size)
+      : [...currentSizes, size];
+
+      searchParams.set('page', '1')
+      searchParams.set('sizes', newSizes.join(','))
+      setSearchParams(searchParams);
+  }
+
+  const handlePriceCHange= (price: string) => {
+      searchParams.set('page', '1');
+      searchParams.set('price', price);
+      setSearchParams(searchParams);
+  }
 
   const sizes = [
     { id: "xs", label: "XS" },
@@ -13,7 +36,6 @@ export const FilterSidebar = () => {
     { id: "xl", label: "XL" },
     { id: "xxl", label: "XXL" },
   ];
-
   return (
     <div className="w-64 space-y-6">
       <div>
@@ -27,9 +49,10 @@ export const FilterSidebar = () => {
           {sizes.map((size) => (
             <Button
               key={size.id}
-              variant="outline"
+              variant={currentSizes.includes(size.id) ? 'default' : 'outline'}
               size="sm"
               className="h-8"
+              onClick={() => handleSizeChange(size.id)}
             >
               {size.label}
             </Button>
@@ -44,23 +67,23 @@ export const FilterSidebar = () => {
         <h4 className="font-medium">Precio</h4>
         <RadioGroup defaultValue="" className="space-y-3">
           <div className="flex items-center space-x-2">
-            <RadioGroupItem value="any" id="priceAny" />
+            <RadioGroupItem value="any" id="priceAny" checked={currentPrice === 'any'} onClick={() => handlePriceCHange('any')}/>
             <Label htmlFor="priceAny" className="text-sm cursor-pointer">Cualquier precio</Label>
           </div>
           <div className="flex items-center space-x-2">
-            <RadioGroupItem value="0-50" id="price1" />
+            <RadioGroupItem value="0-50" id="price1" checked={currentPrice === '0-50'} onClick={() => handlePriceCHange('0-50')}/>
             <Label htmlFor="price1" className="text-sm cursor-pointer">$0 - $50</Label>
           </div>
           <div className="flex items-center space-x-2">
-            <RadioGroupItem value="50-100" id="price2" />
+            <RadioGroupItem value="50-100" id="price2"  checked={currentPrice === '50-100'} onClick={() => handlePriceCHange('50-100')}/>
             <Label htmlFor="price2" className="text-sm cursor-pointer">$50 - $100</Label>
           </div>
           <div className="flex items-center space-x-2">
-            <RadioGroupItem value="100-200" id="price3" />
+            <RadioGroupItem value="100-200" id="price3" checked={currentPrice === '100-200'} onClick={() => handlePriceCHange('100-200')}/>
             <Label htmlFor="price3" className="text-sm cursor-pointer">$100 - $200</Label>
           </div>
           <div className="flex items-center space-x-2">
-            <RadioGroupItem value="200+" id="price4" />
+            <RadioGroupItem value="200+" id="price4" checked={currentPrice === '200+'} onClick={() => handlePriceCHange('200+')} />
             <Label htmlFor="price4" className="text-sm cursor-pointer">$200+</Label>
           </div>
         </RadioGroup>
