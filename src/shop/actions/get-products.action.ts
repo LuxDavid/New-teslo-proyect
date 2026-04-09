bbfa52a9cf715f56da575b2f6b9b5118a260b892
Diaz Tabares Brayan David <brayandiaz162@gmail.com>
2026-04-08 19:14:03 -0600

    changes in get-products-action y useProducts

--- src/shop/actions/get-products.action.ts
+++ src/shop/actions/get-products.action.ts
@@ -6,25 +6,31 @@ interface Options{
     offset?: number | string;
     sizes?: string;
     gender?: string;
+    minPrice?: number;
+    maxPrice?: number;
+    query?: string;
 }
 
 export const getProductsAction= async(options: Options) : Promise<ProductsResponse>=> {
 
-    const {limit, offset, gender, sizes} = options;
+    const {limit, offset, gender, sizes, minPrice, maxPrice, query } = options;
 
     const {data} = await tesloApi.get<ProductsResponse>('/products', {
         params:{
             limit,
             offset,
             gender,
-            sizes
+            sizes,
+            minPrice,
+            maxPrice,
+            q: query
         }
     });
 
     const productsWithImageUrls= data.products.map(product => ({
         ...product,
         images: product.images.map(
-            image => `${import.meta.env.VITE_API_URL}files/product/${image}`
+            image => `${import.meta.env.VITE_API_URL}/files/product/${image}`
         )
     }))
     
