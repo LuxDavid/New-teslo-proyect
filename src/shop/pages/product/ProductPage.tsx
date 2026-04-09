d5154a6605fa8bb2a419393ef424ec3b9532c772
Diaz Tabares Brayan David <brayandiaz162@gmail.com>
2026-04-09 13:44:27 -0600

    manejo de un estado global con zustand

--- src/shop/pages/product/ProductPage.tsx
+++ src/shop/pages/product/ProductPage.tsx
@@ -1,5 +1,18 @@
+import { Button } from "@/components/ui/button";
+import { useCounterStore } from "@/auth/store/auth.store";
+
 export const ProductPage = () => {
+
+  const { inc, count, dec, incBy } = useCounterStore();
+
   return (
-    <div>ProductPage</div>
+    <>
+      <h1 className="text-3xl font-montserrat">Count:{count}</h1>
+
+      <Button onClick={inc}>+1</Button>
+      <Button onClick={dec}>-1</Button>
+      <Button onClick={() => incBy(5)}>+5</Button>
+
+    </>
   )
 }
