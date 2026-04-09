55069f03d3fbb1c9e0bce25e53f766109b976c95
Diaz Tabares Brayan David <brayandiaz162@gmail.com>
2026-04-09 13:27:55 -0600

    process login with JWT token implemented

--- src/auth/pages/login/LoginPage.tsx
+++ src/auth/pages/login/LoginPage.tsx
@@ -4,14 +4,43 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import placeholder from "@/assets/placeholder .svg";
 import { CustomLogo } from "@/components/custom/CustomLogo"
-import { Link } from "react-router"
+import { Link, useNavigate } from "react-router"
+import { useState, type FormEvent } from "react";
+import { loginAction } from "@/auth/actions/login.action";
+import { toast } from "sonner";
 
 export const LoginPage= () => {
+
+    const navigate= useNavigate();
+    const [isPoasting, setIsPoasting] = useState(false);
+
+    const handleLogin= async(event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        setIsPoasting(true);
+
+        const formData= new FormData(event.target as HTMLFormElement);
+        const email= formData.get('email') as string;
+        const password= formData.get('password') as string;
+
+        try {
+          const data= await loginAction(email, password);
+          localStorage.setItem('token', data.token);
+          console.log('Redireccionando al home');
+          navigate('/')
+        } catch (error) {
+          toast.error('Correo o/y contraseña no validos');
+        }
+
+        setIsPoasting(false);
+
+    }
+
+
   return (
     <div className={"flex flex-col gap-6"} >
       <Card className="overflow-hidden p-0">
         <CardContent className="grid p-0 md:grid-cols-2">
-          <form className="p-6 md:p-8">
+          <form className="p-6 md:p-8" onSubmit={handleLogin}>
 
             <div className="flex flex-col gap-6">
               <div className="flex flex-col items-center text-center">
@@ -21,7 +50,7 @@ export const LoginPage= () => {
 
               <div className="grid gap-2">
                 <Label htmlFor="email">Correo</Label>
-                <Input id="email" type="email" placeholder="m@example.com" required />
+                <Input id="email" type="email" placeholder="m@example.com" name="email" required />
               </div>
 
               <div className="grid gap-2">
@@ -33,10 +62,10 @@ export const LoginPage= () => {
                   </Link>
                 </div>
 
-                <Input id="password" type="password" required placeholder="contraseña" />
+                <Input id="password" type="password" required placeholder="contraseña" name="password"/>
               </div>
 
-              <Button type="submit" className="w-full">
+              <Button type="submit" className="w-full" disabled={isPoasting}>
                 Ingresar
               </Button>
 
